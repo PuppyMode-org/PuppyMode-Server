85eaa5c37466a1badcefe536cba9ef1c53c875e2
alschlee <1daysixmeal@gmail.com>
2025-01-01 04:28:57 +0900

    first commit

---
+++ src/main/java/umc/puppymode/PuppymodeApplication.java
@@ -0,0 +1,13 @@
+package umc.puppymode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppymodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppymodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/puppymode/PuppymodeApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.puppymode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppymodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
