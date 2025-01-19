d008a739d5043be41759afc6447b24fcafb2c589
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-01-19 19:23:45 +0900

    [FEAT] 사용자 현재 위치 조회하기 API #19

---
+++ src/main/java/umc/puppymode/web/dto/LocationResponseDTO.java
@@ -0,0 +1,11 @@
+package umc.puppymode.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LocationResponseDTO {
+    private Double latitude;
+    private Double longitude;
+}
