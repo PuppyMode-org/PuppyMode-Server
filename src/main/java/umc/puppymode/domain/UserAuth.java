e868c2bb71c4282c9e157299001bc14c14c59af9
eldeoddt <eldeoddt@gmail.com>
2025-02-01 18:03:12 +0900

    [REFACTOR] UserAuth 엔티티 수정 #101

---
+++ src/main/java/umc/puppymode/domain/UserAuth.java
@@ -0,0 +1,34 @@
+package umc.puppymode.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserAuth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userAuthId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "user_auth_provider_user"))
+    private User user;
+
+    private String authProvider;
+
+    private String authId;
+
+    @Column(name = "refresh_token", columnDefinition = "TEXT")
+    private String refreshToken;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
