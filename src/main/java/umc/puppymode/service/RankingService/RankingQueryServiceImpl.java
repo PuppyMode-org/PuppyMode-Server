6dad1621befa9d52b6d3a8596b558b9854ae0325
eldeoddt <eldeoddt@gmail.com>
2025-02-01 16:40:09 +0900

    [REFACTOR] exception 메시지에 userId 제거 #101

--- src/main/java/umc/puppymode/service/RankingService/RankingQueryServiceImpl.java
+++ src/main/java/umc/puppymode/service/RankingService/RankingQueryServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
