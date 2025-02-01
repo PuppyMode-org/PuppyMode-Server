855c46bbb47c90367646b3800097a9fd7b3c903d
eldeoddt <eldeoddt@gmail.com>
2025-02-01 19:31:19 +0900

    [REFACTOR] controller swagger 수정 #101

--- src/main/java/umc/puppymode/web/controller/RankingController.java
+++ src/main/java/umc/puppymode/web/controller/RankingController.java
@@ -23,9 +23,12 @@ public class RankingController {
     @GetMapping("/friends")
     @Operation(summary = "카카오 친구 랭킹 조회 API", description = "친구인 사용자 랭킹을 조회하는 API입니다.")
     public ResponseEntity<ApiResponse<RankingResponseDTO>> getFriendRankings(
-            @Parameter(description = "Kakao Id list") @RequestParam List<String> authIds,
-            @Parameter(description = "페이지 번호 (0부터 시작)", example = "0") @RequestParam int page,
-            @Parameter(description = "한 페이지당 데이터 수", example = "10") @RequestParam int size) {
+            @Parameter(description = "Kakao 친구 Id 목록 (개별 입력)", example = "authIds=123456&authIds=654321")
+            @RequestParam List<String> authIds,
+            @Parameter(description = "페이지 번호 (0부터 시작)", example = "0")
+            @RequestParam int page,
+            @Parameter(description = "한 페이지당 데이터 수", example = "10")
+            @RequestParam int size) {
 
         Long userId = userAuthService.getCurrentUserId();
 
